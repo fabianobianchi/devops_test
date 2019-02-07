00d6780cb59afaa179858e74fd11efa7b1f35bb8
fabianobianchi <38506285+fabianobianchi@users.noreply.github.com>
2019-02-07 00:12:25 -0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -42,7 +42,8 @@ variable "ebs_volume_size" {
 variable "elb_availability_zones" {
   description   = "Availability zones which ELB will distribute the traffic"   
 }
-variable "ec2tossm_role_file" {
+/*
+  variable "ec2tossm_role_file" {
   description   = "Json file name to create a role which allow EC2 Jenkins instance to access SSM"
   default       = "${path.module}/ec2toSSM_role.json"
 }
@@ -50,3 +51,4 @@ variable "ec2tossm_policy_file" {
   description   = "Json file name to create a policy which allow EC2 Jenkins instance to access SSM"
   default       = "${path.module}/ec2toSSM_policy.json"
 }
+*/
