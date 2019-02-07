f591f886cb3efd0c1ded606f5a16e3e776a4ad3c
fabianobianchi <38506285+fabianobianchi@users.noreply.github.com>
2019-02-07 10:45:35 -0200

    Update module_jenkins.tf

--- module_jenkins.tf
+++ module_jenkins.tf
@@ -1,8 +1,7 @@
 module "Ci-cd" {
   source                    = "git@github.com:fabianobianchi/devops_test.git//module"
   aws_region                = "us-east-1"
-  ec2_availability_zone     = "us-east-1a"
-  elb_availability_zones    = "us-east-1a"
+  availability_zone         = "us-east-1a"
   vpc_id                    = "vpc-98cab1e3"
   subnet_id                 = "subnet-00978e2f"
   key_pair_name             = "Singapura"
